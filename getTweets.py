a41aae827cbc13777387141add06eef3af1f5d6b
Anmol Sharma <anmolsh.1908@gmail.com>
2021-06-18 14:36:15 +0530

    added functionality to select type and count of tweets

--- getTweets.py
+++ getTweets.py
@@ -6,13 +6,24 @@ import re
 print("Welcome to Twitter Sentiment Analysis Program\n\n")
 print("Enter a string to search for ")
 query = str(input())
+print("What type of tweets to search for? (popular/top/mixed)")
+type = str(input())
+type = type.lower()
+if(type != "popular" and type != "top" and type != "mixed"):
+    print("Wrong type entered!")
+    exit()
+print("Entered the number of tweets to fetch")
+count = int(input())
+if(count <= 0):
+    print("Error: count should be greater than 0")
+    exit()
 
 auth = tweepy.OAuthHandler(api_keys.consumerKey, api_keys.consumerKeyPrivate)
 auth.set_access_token(api_keys.accessKey, api_keys.accessKeyPrivate)
 api = tweepy.API(auth)
 
-posts = api.search(q=query, count=5, lang="en",
-                   result_type="popular", tweet_mode="extended")
+posts = api.search(q=query, count=count, lang="en",
+                   result_type=type, tweet_mode="extended")
 
 df = pd.DataFrame([tweet.full_text for tweet in posts], columns=["Tweets"])
 
